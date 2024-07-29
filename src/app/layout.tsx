2507e67ccb227f6c250a99a0a0a142d03d1a0afa
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2024-07-29 09:54:42 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable}`}>
-      <GoogleAnalytics gtmId="G-1QHVWLNL22" />
+      <GoogleAnalytics gaId="G-1QHVWLNL22" />
       <body className={poppins.className}>
         <NavBar />
         {children}
